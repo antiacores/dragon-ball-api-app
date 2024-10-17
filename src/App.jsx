87b92b70b89a635ba693fb3a0f4a0365d0e9ef97
Antía Cores <antiacores@MacBook-Air-de-Antia.local>
2024-10-16 18:54:03 -0600

    api consumption

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import { Gallery } from "./components/Gallery"
+
 function App() {
 
   return (
     <>
-    <div className="min-h-screen bg-red-950 flex items-center justify-center">
+    <div className="min-h-screen bg-red-950 flex flex-col items-center justify-center">
       <h1 className="text-3xl font-bold underline">
         DRAGON BALL
       </h1>
+      <Gallery />
     </div>
     </>
   )
